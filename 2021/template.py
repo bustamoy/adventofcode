dfd9770d67390239e62c2d06849d43e0b0bbd23d
chris-moy <chris.moy.github@gmail.com>
2021-12-06 12:21:44 -0600

    2021 template

---
+++ 2021/template.py
@@ -0,0 +1,44 @@
+# Advent Of Code
+# https://adventofcode.com/2021/day/{DAY}
+# https://adventofcode.com/2021/day/{DAY}#part2
+# (NOT) COMPLETE
+
+from os.path import exists
+from typing import List
+
+YEAR: int = 2021
+DAY: int = 0
+
+
+def get_input() -> List[int]:
+    result = None
+    filename = f'input-{YEAR}-{DAY}'
+    if exists(filename):
+        with open(filename, 'r') as input_file:
+            result = [line for line in input_file.read().splitlines()]
+    else:
+        raise FileNotFoundError(f"Did not find input file: '{filename}'.")
+    return result
+
+
+def part_one(data):
+    return None
+
+
+def part_two(data):
+    return None
+
+
+def main():
+    example = None
+    input_data = get_input()
+
+    assert part_one(example)
+    print(f'Part 1 Answer:\n{part_one(input_data)}')
+
+    assert part_two(example)
+    print(f'Part 2 Answer:\n{part_two(input_data)}')
+
+
+if __name__ == '__main__':
+    main()
